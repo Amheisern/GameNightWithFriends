5e63092acd4bd9b9d49dbb15db3d39063e6447d5
lodashreload <icarusjapan@gmail.com>
2022-02-03 18:47:22 -0500

    added player class and added player to  DatabaseContext

---
+++ Models/Player.cs
@@ -0,0 +1,12 @@
+namespace GameNightWithFriends.Models
+{
+    public class Player
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GameNightId { get; set; }
+        public GameNight GameNight { get; set; }
+
+    }
+}
